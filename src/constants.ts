51c74d250acb6ecc6639b1b17c8b62921749d59c
Taye Adeyemi <dev@taye.me>
2021-09-05 10:20:54 +0200

    chore: rename to vitest

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const PLUGIN_NAME = 'vite-jasmine'
+export const PLUGIN_NAME = 'vitest'
 export const INTERNAL_SYMBOL_NAME = PLUGIN_NAME + '-internal'
 export const INTERNAL = Symbol.for(INTERNAL_SYMBOL_NAME)
 export const DEFAULT_SPEC_PATTERN = '**/*.spec.{t,j}s{,x}'
-export const HOST_BASE_PATH = '/@jasmine'
+export const HOST_BASE_PATH = '/@vitest'
 export const URL_RE = RegExp(`^${HOST_BASE_PATH.replace(/\//g, '[/]')}[/]?([^?]*)([?].*)?`)
 export const LAUNCH_SYMBOL = Symbol.for(PLUGIN_NAME + '-launch')
