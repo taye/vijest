be3a24ab6c5ac4399c6f0b094f71fd9237937a31
Taye Adeyemi <dev@taye.me>
2021-09-09 15:34:41 +0200

    feat(jest/environment): don't extend node

--- src/jest/environment.ts
+++ src/jest/environment.ts
@@ -1,14 +1,30 @@
-import NodeEnvironment from 'jest-environment-node'
+import { createContext } from 'vm'
+
+import type { Config } from '@jest/types'
+import { ModuleMocker } from 'jest-mock'
 
 import { INTERNAL } from '../constants'
 
-export default class Environment extends NodeEnvironment {
+export default class Environment {
   static __filename = __filename;
+
   // allow distinction of vitest environment
   [INTERNAL] = true
 
-  constructor (...args: any[]) {
-    // @ts-expect-error
-    super(...args)
+  global? = {} as typeof globalThis
+  moduleMocker? = new ModuleMocker(this.global!)
+  context? = createContext(this.global)
+
+  constructor (public config: Config.ProjectConfig) {}
+
+  getVmContext () {
+    return this.global
+  }
+
+  setup () {}
+  teardown () {
+    this.global = undefined
+    this.moduleMocker = undefined
+    this.context = undefined
   }
 }
